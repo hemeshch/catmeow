6b3da887f899aff30b5de10a0961249efc248963
Hemesh Chadalavada <55226601+hemesh2006@users.noreply.github.com>
2020-12-22 12:54:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let image = document.getElementById('image')
+let fact = document.getElementById('fact')
+let button = document.getElementById('button')
+
+fetch('https://api.thecatapi.com/v1/images/search')
+	.then(response => response.json())
+	.then((data) => {
+			image.src = data[0].url
+			})
+
+button.addEventListener('click', () => {
+	fetch('https://api.thecatapi.com/v1/images/search')
+		.then(response => response.json())
+		.then((data) => {
+				image.src = data[0].url
+				})
+
+	fetch('https://cat-fact.herokuapp.com/facts/random')
+	.then(response => response.json())
+	.then((factData) => {
+		fact.innerText = factData.text
+		console.log(factData.text)
+		})
+})
